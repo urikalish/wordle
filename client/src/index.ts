3b02ccedff7ec108f0df79f2f7ec1d3719e53ccd
kalish <uri.kalish@gmail.com>
2023-10-21 23:22:23 +0300

    wip

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,1 +1,8 @@
-alert('hello');
+import { words } from './words';
+
+const fiveLetterWords = words.filter(w => w.length === 5);
+//alert(fiveLetterWords.length);
+
+fiveLetterWords.forEach((w, ind) => {
+	console.log(ind + ' ' + w);
+});
