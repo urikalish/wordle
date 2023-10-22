54d4a9c5cbdd81840d6ede961c1b4f8d9b77e7ad
kalish <uri.kalish@gmail.com>
2023-10-22 17:34:46 +0300

    wip

--- client/src/index.ts
+++ client/src/index.ts
@@ -5,7 +5,7 @@ function setWidthAndHeight() {
 	document.documentElement.style.setProperty('--main-height', `${h}px`);
 	const w = Math.min(window.innerWidth, h / 2);
 	document.documentElement.style.setProperty('--main-width', `${w}px`);
-	document.documentElement.style.setProperty('--keyboard-button-width', `${w / 10}px`);
+	document.documentElement.style.setProperty('--keyboard-button-width', `${w / 11}px`);
 }
 
 function createUI() {
@@ -15,7 +15,7 @@ function createUI() {
 			const cellElm = document.createElement('div');
 			cellElm.setAttribute('id', `cell-${r}${c}`);
 			cellElm.classList.add('grid-item');
-			cellElm.textContent = 'X';
+			cellElm.textContent = '-';
 			mainGridElm?.appendChild(cellElm);
 		}
 	}
