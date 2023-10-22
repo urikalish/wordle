5874d775730ebb17423ea8f76e2ff47c9f9dfbc4
kalish <uri.kalish@gmail.com>
2023-10-22 16:17:30 +0300

    wip

--- client/src/index.ts
+++ client/src/index.ts
@@ -6,15 +6,33 @@ function setWidthAndHeight() {
 }
 
 function createUI() {
-	const mainGridElm = document.getElementById('main-grid');
+	const mainGridElm = document.getElementById('grid');
 	for (let r = 0; r < 6; r++) {
 		for (let c = 0; c < 5; c++) {
-			const elm = document.createElement('div');
-			elm.setAttribute('id', `cell-${r}${c}`);
-			elm.classList.add('main-grid-item');
-			elm.textContent = 'X';
-			mainGridElm?.appendChild(elm);
+			const cellElm = document.createElement('div');
+			cellElm.setAttribute('id', `cell-${r}${c}`);
+			cellElm.classList.add('grid-item');
+			cellElm.textContent = 'X';
+			mainGridElm?.appendChild(cellElm);
 		}
+	}
+	const keys = [
+		['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+		['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+		['Z', 'X', 'C', 'V', 'B', 'N', 'M', '<<']
+	];
+	const keyboardElm = document.getElementById('keyboard');
+	for (let r = 0; r < 3; r++) {
+		const rowElm = document.createElement('div');
+		rowElm.classList.add('keyboard-row');
+		for (let k = 0; k < keys[r].length; k++) {
+			const keyElm = document.createElement('button');
+			keyElm.setAttribute('id', `key-${keys[r][k]}`);
+			keyElm.classList.add('keyboard-button');
+			keyElm.textContent = keys[r][k];
+			rowElm.appendChild(keyElm);
+		}
+		keyboardElm?.appendChild(rowElm)
 	}
 }
 
